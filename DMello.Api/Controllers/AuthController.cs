96f24c4617edada463f8d745d7988e8d4ad1fb98
Ashwin Sharma <sharmashwin818@gmail.com>
2026-08-30 13:06:20 +0530

    Login Refresh Token with Functionality

--- DMello.Api/Controllers/AuthController.cs
+++ DMello.Api/Controllers/AuthController.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.Data;
+﻿using DMello.Application.Auth; //Now DMello.Api depends on Application Layer
+using DMello.Application.Auth.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using DMello.Application.Auth; //Now DMello.Api depends on Application Layer
-using DMello.Application.Auth.DTOs;
 
 namespace DMello.Api.Controllers
 {
-
     [ApiController]
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
@@ -20,6 +20,7 @@ namespace DMello.Api.Controllers
             _authService = authService;
         }
 
+        #region Normal ShortTerm Token login
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody]LoginRequestDto request)
         {
@@ -29,7 +30,31 @@ namespace DMello.Api.Controllers
 
             return Ok(response);
         }
+        #endregion
 
+        #region Refresh Token Login
+        [HttpPost("login")]
+        public async Task<IActionResult> Refresh_Token_Login([FromBody] LoginRequestDto request)
+        {
+            var response = await _authService.LoginAsync(request);
+            if (response == null)
+                return Unauthorized(new { message = "Invalid credentials" });
+
+            // Set JWT in HttpOnly Cookie
+            Response.Cookies.Append("X-Access-Token", response.Token, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true, // Set to true in HTTPS
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.UtcNow.AddHours(2)
+            });
+
+            return Ok(new { email = response });// we will not send email as response response.Email 
+        }
+
+        #endregion
+
+        #region New User Registration
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
         {
@@ -40,5 +65,6 @@ namespace DMello.Api.Controllers
 
             return Ok(result);
         }
+        #endregion
     }
 }
